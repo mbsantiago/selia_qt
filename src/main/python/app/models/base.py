2ddddd5c9662c666e022366415b59a1bbebd5914
mbsantiago <santiago.mbal@gmail.com>
2019-04-18 13:19:18 -0500

    Model abstract model

--- src/main/python/app/models/base.py
+++ src/main/python/app/models/base.py
@@ -1,10 +1,35 @@
-from abc import ABCMeta
+from abc import ABC
 from abc import abstractmethod
 
 
-class Model():
-    __metaclass__ = ABCMeta
+
+class Field(ABC):
+    @property
+    @abstractmethod
+    def required(self):
+        pass
+
+    @property
+    @abstractmethod
+    def type(self):
+        pass
+
+
+class Model(ABC):
+    @classmethod
+    def name(cls):
+        return str(cls.__name__).lower()
 
     @abstractmethod
     def act(self, action):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_type_info(cls):
+        pass
+
+    @classmethod
+    @abstractmethod
+    def validate(self, data):
         pass
