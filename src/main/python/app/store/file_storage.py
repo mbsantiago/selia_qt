dbd8781c28a84f2574cca4d2ecb8be32a6edd333
mbsantiago <santiago.mbal@gmail.com>
2019-04-17 20:13:24 -0500

    Santiago's personal branch

---
+++ src/main/python/app/store/file_storage.py
@@ -0,0 +1,10 @@
+"""
+Storage to file handlers
+"""
+from app.settings import settings
+
+
+class FileStorage():
+    """File storage handler"""
+    def __init__(self, path):
+        pass
---
+++ src/main/python/app/store/history.py
@@ -0,0 +1,3 @@
+class History(object):
+    def __init__(self):
+        self.action_history = []
